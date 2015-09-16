2308425489ac6f1c5a5f7de652ffc804f0d08051
Andrew Brown <drusepth@gmail.com>
2015-09-15 23:10:12 -0500

    Remove debug lines

--- bot.rb
+++ bot.rb
@@ -43,31 +43,19 @@ reddit_thread = Thread.new do
   reddit = RedditKit::Client.new 'wizard_of_dong', 'dongdong'
   $bot.channels.first.send "Ready to start watching reddit! :)"
   while true
-    puts "Fetching newest front page comments"
     comments = reddit.front_page.flat_map { |link| reddit.comments link }
-    puts "Fetched #{comments.length} comments!"
     comments.compact.each do |comment|
-      puts "Debug: checking #{comment.body}"
-      puts "-"*20
       next if $triggered.include? comment.id
 
       $triggers.each do |trigger|
-        puts "CHECKING TRIGGER #{trigger}"
         if comment.body =~ /#{trigger}/
-          puts "FOUND A TRIGGER MATCH"
           $bot.channels.first.send "A comment by #{comment.author} matched /#{trigger}/: #{comment.body.truncate(100)}"
-          puts "triggered"
           $triggered << comment.id
-          puts "triggered x2"
         end
-        puts "No match"
       end
-      puts "next comment?"
     end
 
-    puts "Sleepin'"
-    sleep 30
-    puts "Waking up"
+    sleep 60
   end
 end
 
